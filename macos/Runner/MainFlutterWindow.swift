0fa493f989ac499d6d7ab926ebd95124de837aee
Pradyx <pradyumnbhushan@gmail.com>
2026-03-18 09:01:08 +0530

    feat: Add macOS support

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -9,6 +9,28 @@ class MainFlutterWindow: NSWindow {
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
+    
+    let storageChannel = FlutterMethodChannel(name: "pocket_llm/storage_info", binaryMessenger: flutterViewController.engine.binaryMessenger)
+    storageChannel.setMethodCallHandler { (call, result) in
+      if call.method == "getStorageInfo" {
+        do {
+          let fileURL = URL(fileURLWithPath: NSHomeDirectory())
+          let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey, .volumeAvailableCapacityKey, .volumeTotalCapacityKey])
+          
+          let free = values.volumeAvailableCapacityForImportantUsage ?? Int64(values.volumeAvailableCapacity ?? 0)
+          let total = values.volumeTotalCapacity ?? 0
+          
+          result([
+            "freeBytes": free,
+            "totalBytes": total
+          ])
+        } catch {
+          result(FlutterError(code: "UNAVAILABLE", message: "Could not get storage info", details: nil))
+        }
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
 
     super.awakeFromNib()
   }
